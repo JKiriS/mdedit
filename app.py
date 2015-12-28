04e1c9e9715f75f419b19ad07e84b492afdb88c4
JKiriS <jkzgyb@gmail.com>
2015-12-28 13:50:49 +0800

    add simple login

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import sys
 import datetime
 import json
 import traceback
+import functools
+import json
+import uuid
+import hashlib
 
 import tornado.ioloop
 import tornado.web
@@ -20,10 +24,73 @@ DOC_ROOT = cfg['doc_root']
 FILE_ROOT = cfg['file_root']
 
 
+class Session(object):
+    def __init__(self, db, sid=None):
+        self.db = db
+        self.sid = str(sid)
+
+        if not sid:
+            self.sid = str(uuid.uuid4())
+        
+        self._s = self.db.session.find_one({'_id': self.sid})
+        if not self._s:
+            self._s = {}
+
+    def get(self, key):
+        return self._s.get(key)
+
+    def set(self, key, value):
+        self._s[key] = value
+        self.db.session.update_one({'_id': self.sid}, {'$set': {key: value}}, upsert=True)
+
+    def pop(self, key):
+        try:
+            self._s.pop(key)
+        except:
+            pass
+        self.db.session.update_one({'_id': self.sid}, {'$unset': {key: True}})
+
+
 class BaseHandler(tornado.web.RequestHandler):
     @property
     def db(self):
         return self.application.mongo
+
+    def initialize(self):
+        session_id = self.get_secure_cookie("_session_id")
+        self.session = Session(self.db, session_id)
+
+    def get_current_user(self):
+        return self.session.get('user')
+
+
+def user_authenticated(func):
+    @functools.wraps(func)
+    def wrapper(self, *args, **kwargs):
+        if not self.current_user:
+            if self.request.method in ("GET", "HEAD"):
+                url = self.get_login_url()
+                if "?" not in url:
+                    if urlparse.urlsplit(url).scheme:
+                        # if login url is absolute, make next absolute too
+                        next_url = self.request.full_url()
+                    else:
+                        next_url = self.request.uri
+                    url += "?" + urlencode(dict(next=next_url))
+                self.redirect(url)
+                return
+            raise tornado.web.HTTPError(403)
+        return func(self, *args, **kwargs)
+    return wrapper
+
+
+def admin_authenticated(func):
+    @functools.wraps(func)
+    def wrapper(self, *args, **kwargs):
+        if self.current_user and self.current_user['role'] == 0:
+            return func(self, *args, **kwargs)
+        raise tornado.web.HTTPError(403)
+    return wrapper
 
 
 class IndexHandler(BaseHandler):
@@ -86,6 +153,7 @@ class ArticleHandler(BaseHandler):
             raise tornado.web.HTTPError(404)
         self.render('edit.html', article=article)
 
+    @user_authenticated
     def post(self, aid):
         title = self.get_argument('title')
         content = self.get_argument('content')
@@ -133,10 +201,40 @@ class ArticleHandler(BaseHandler):
 
 class LoginHandler(BaseHandler):
     def get(self):
-        self.render('index.html')
+        self.render('login.html')
 
     def post(self):
-        pass
+        self.login_user(self.get_argument("email"), self.get_argument("password"))
+
+    def login_user(self, email, password):
+        user = self.db.user.find_one({'email': email})
+        res = dict(status='ok')
+        if not user:
+            res['status'] = 'error'
+            res['error'] = '账户尚未注册'
+        else:
+            if self.check_password(password, user):
+                user.pop('password')
+                self.session.set('user', user)
+                self.set_secure_cookie("_session_id", self.session.sid)
+                res['redirect'] = self.get_argument("next", "/")
+            else:
+                res['status'] = 'error'
+                res['error'] = '密码错误'
+        self.write(json.dumps(res))
+
+    def check_password(self, password, user):
+        return hashlib.md5(password).hexdigest() == user['password']
+
+
+class LogoutHandler(BaseHandler):
+    @user_authenticated
+    def get(self):
+        self.logout()
+        self.redirect(self.get_argument("next", "/"))
+
+    def logout(self):
+        self.session.pop('user')
 
 
 class ItemListHandler(BaseHandler):
@@ -161,6 +259,7 @@ class DirListHandler(BaseHandler):
 
 
 class NewDirHandler(BaseHandler):
+    @user_authenticated
     def post(self):
         parent = self.get_argument('parent', ROOT)
         title = self.get_argument('title', '新建归档')
@@ -178,6 +277,7 @@ class NewDirHandler(BaseHandler):
 
 
 class NewDocHandler(BaseHandler):
+    @user_authenticated
     def post(self):
         parent = self.get_argument('parent', ROOT)
         title = self.get_argument('title', '新建文档')
@@ -198,6 +298,7 @@ class NewDocHandler(BaseHandler):
 
 
 class RenameHandler(BaseHandler):
+    @user_authenticated
     def post(self):
         target = self.get_argument('target')
         title = self.get_argument('title')
@@ -213,6 +314,7 @@ class RenameHandler(BaseHandler):
 
 
 class DeleteHandler(BaseHandler):
+    @user_authenticated
     def post(self):
         targets = self.get_arguments('targets[]')
         completely = self.get_argument('completely', False)
@@ -220,7 +322,10 @@ class DeleteHandler(BaseHandler):
         res = dict(status='ok')
         try:
             targets = map(lambda tid: ObjectId(tid), targets)
-            self.db.item.update_many({'_id': {'$in': targets}}, {'$set':{'parent': BIN_ROOT}})
+            if completely == 'true':
+                self.db.item.delete_many({'_id': {'$in': targets}})
+            else:
+                self.db.item.update_many({'_id': {'$in': targets}}, {'$set':{'parent': BIN_ROOT}})
         except:
             res['status'] = 'error'
             res['error'] = traceback.format_exc()
@@ -229,6 +334,7 @@ class DeleteHandler(BaseHandler):
 
 
 class MoveHandler(BaseHandler):
+    @user_authenticated
     def post(self):
         target = self.get_argument('target')
         sources = self.get_arguments('sources[]')
@@ -251,7 +357,7 @@ class Application(tornado.web.Application):
             static_path=os.path.join(os.path.dirname(__file__), "static"),
             xsrf_cookies=True,
             cookie_secret=cfg['cookie_secret'],
-            login_url="/account/login",
+            login_url="/login",
             autoload=False,
             debug=True,
         )
@@ -261,7 +367,8 @@ class Application(tornado.web.Application):
 
             (r"/(favicon\.ico)", tornado.web.StaticFileHandler, dict(path=settings['static_path'])),
 
-            (r"/account/login", LoginHandler),
+            (r"/login", LoginHandler),
+            (r"/logout", LogoutHandler),
 
             (r"/upload", UploadHandler),
 
